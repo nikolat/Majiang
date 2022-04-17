0756a888765be171d25f31fd70647dc5d9e83eee
Don <evidence@384.jp>
2022-04-17 19:44:50 +0900

    対局2回目ができない不具合を修正

--- src/js/index.js
+++ src/js/index.js
@@ -12,40 +12,41 @@
 const { hide, show, fadeIn } = require('./majiang/view/fadein');
 
 let loaded;
-let setReceivePlayerInfo = false;
 
 $(function(){
     let game, paipu;
 
+    window.api.ReceivePlayerInfo((data) => {
+        const names = data[0];
+        const hwnds = data[1];
+        start_base(names, hwnds);
+    });
+    
     function start() {
-        if (!setReceivePlayerInfo) {
-            window.api.ReceivePlayerInfo((data) => {
-                const names = data[0];
-                const hwnds = data[1];
-                $('#game > .player').hide();
-                $('body').attr('class','game');
-                game = new Majiang.Game();
-                new Majiang.View.GameCtl($('#game'), game);
-                game._player = [
-                    new Majiang.View.Player(0, $('#game')),
-                    new Majiang.Player(1),
-                    new Majiang.Player(2),
-                    new Majiang.Player(3),
-                ];
-                for (let i = 0; i < names.length; i++) {
-                    let n = (i + 1) % 4;
-                    game._model.player[n] = names[i];
-                    game._player[n]._hwnd = hwnds[i];
-                    if (i == 3) {
-                        break;
-                    }
-                }
-                game._callback = end;
-                game.kaiju();
-            });
-            setReceivePlayerInfo = true;
-        }
         window.api.RequestPlayerInfo();
+    }
+
+    function start_base(names, hwnds) {
+        $('#game > .player').hide();
+        $('body').attr('class','game');
+        game = new Majiang.Game();
+        new Majiang.View.GameCtl($('#game'), game);
+        game._player = [
+            new Majiang.View.Player(0, $('#game')),
+            new Majiang.Player(1),
+            new Majiang.Player(2),
+            new Majiang.Player(3),
+        ];
+        for (let i = 0; i < names.length; i++) {
+            let n = (i + 1) % 4;
+            game._model.player[n] = names[i];
+            game._player[n]._hwnd = hwnds[i];
+            if (i == 3) {
+                break;
+            }
+        }
+        game._callback = end;
+        game.kaiju();
     }
 
     function end() {
